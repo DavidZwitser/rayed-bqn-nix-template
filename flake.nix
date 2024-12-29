055327c01737ac5bf68a2df02411e0197a795254
davidzwitser <davidzwitser@gmail.com>
2024-12-29 13:11:41 +0100

    Added cool flake stuff and removed rayed-bqn submodule

--- flake.nix
+++ flake.nix
@@ -5,9 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     bqnlsp.url = "sourcehut:~detegr/bqnlsp";
-    rayed-bqn.url = "github:DavidZwitser/rayed-bqn";
-    # Get specific verion of raylib
-    # raylib.url = "github:"
+    rayed-bqn.url = "/tmp/rayed-bqn";
   };
 
   outputs = { self, nixpkgs, flake-utils, bqnlsp, rayed-bqn}:
@@ -15,21 +13,35 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       bqnlspPkg = bqnlsp.packages.${system}.lsp;
-
+      rayedBqn = rayed-bqn.packages.${system}.default;
       cbqn = pkgs.cbqn-replxx;
-      raylib = pkgs.raylib;
-    in rec {
 
-      packages.default = pkgs.writeShellScriptBin "run" /*bash*/ ''
-        # Setting up rayed-bqn and its submodules
-        git submodule update --init --recursive
+      name = "rayed-bqn-project"; # Give own name
+      entryFile = "main.bqn"; # Entry file in src
+    in {
 
-        mkdir ./rayed-bqn/lib
-        ln -sf ${raylib}/lib/libraylib.dylib ./rayed-bqn/lib/libraylib.dylib
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = name;
+        version = "0.1.0";
+        src = ./.;
 
-        # Running the program
-     	  ${cbqn}/bin/bqn -f ./src/main.bqn
-      '';
+        buildInputs = [ cbqn ];
+        buildPhase = ''
+          mkdir -p $out
+          cp -r ./src $out
+          ln -sf ${rayedBqn} $out/rayed-bqn
+
+          mkdir -p $out/bin
+          echo "#!/bin/bash" > $out/bin/${name}
+          echo "${pkgs.cbqn-replxx}/bin/cbqn $out/src/${entryFile}" >> $out/bin/${name}
+          chmod +x $out/bin/${name}
+        '';
+      };
+
+      apps.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/${name}";
+      };
 
       devShells.default = pkgs.mkShell {
         buildInputs = [
@@ -37,6 +49,10 @@
           bqnlspPkg
           pkgs.nixd
         ];
+
+        shellHook = ''
+          ln -sf ${rayedBqn} ./rayed-bqn
+        '';
       };
   });
 }
